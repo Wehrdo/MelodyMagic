0fbf9fde06c4108a50052766a8a66356ac0d8de8
David Wehr <wehr_d@hotmail.com>
2018-03-24 13:11:21 -0500

    added bass, untested

--- source/core.py
+++ source/core.py
@@ -5,10 +5,12 @@ import mido
 import ChordGen
 from melody import MelodyGen
 import DrumMachine
+import Bassline
 
 chan_melody = 0
 chan_harmony = 1
-chan_percussion = 3
+chan_percussion = 2
+chan_bass = 3
 
 def make_chord_msgs(chord, key, vel=100, transposition=0, channel=1):
     messages = []
@@ -28,18 +30,23 @@ def run(out_port):
     s_per_sixteenth = 60 / (4 * tempo)
     ChordGen.init()
     DrumMachine.init()
+    Bassline.init()
     while True:
         chord = ChordGen.get_next()
         for msg in make_chord_msgs(chord, key, 100, transposition, chan_harmony):
             out_port.send(msg)
 
         melody_notes, melody_rhythms = melody_gen.get_next(chord)
-
+        bass_notes, bass_rhythms = Bassline.get_next(chord)
         drum_notes, drum_rhythms = DrumMachine.get_next()
+
         drum_idx = 0
         melody_idx = 0
         melody_dur_remaining = 0
         melody_off_msg = None
+        bass_idx = 0
+        bass_dur_remaining = 0
+        bass_off_msg = None
         for sixteenth in range(16):
             # Percussion
             if drum_idx < len(drum_rhythms) and drum_rhythms[drum_idx] == sixteenth:
@@ -62,9 +69,22 @@ def run(out_port):
                 melody_idx += 1
 
 
-
-            time.sleep(s_per_sixteenth)
-            melody_dur_remaining -= 1
+            # Bass
+            # if bass_dur_remaining == 0:
+            #     if bass_off_msg is not None:
+            #         out_port.send(bass_off_msg)
+            #     note_pair = bass_notes[bass_idx]
+            #     midi_note = octave_to_note(note_pair[1], note_pair[0])
+            #     start_msg = make_chord_msgs([midi_note], key, 100, transposition, chan_bass)[0]
+            #     out_port.send(start_msg)
+            #
+            #     bass_off_msg = make_chord_msgs([midi_note], key, 0, transposition, chan_bass)[0]
+            #
+            #     bass_dur_remaining = bass_rhythms[bass_idx]
+            #     bass_idx += 1
+            #
+            # time.sleep(s_per_sixteenth)
+            # melody_dur_remaining -= 1
 
 
         for msg in make_chord_msgs(chord, key, 0, transposition):
@@ -75,7 +95,7 @@ if __name__ == '__main__':
     with mido.open_output('loopMIDI Port 1') as port:
         def signal_handler(signal, frame):
             print("sending")
-            for chan in [chan_percussion, chan_harmony, chan_melody]:
+            for chan in [chan_percussion, chan_harmony, chan_melody, chan_bass]:
                 for note in range(0, 128):
                     port.send(mido.Message('note_off', note=note, channel=chan, velocity=0))
             sys.exit(0)
