ce40a8fb7ed6e4161126f29a20d2ed0aac7139ae
Eric Middleton <ericm@iastate.edu>
2018-03-24 13:57:58 -0500

    Updated drum machine

--- source/DrumMachine.py
+++ source/DrumMachine.py
@@ -1,18 +1,52 @@
 ####Instruments###
-#Kick drum: F2          41
-#Cowbell C#3            49
-#Snare D#4              63
-#Clap C#-1              1
-#Scrape'n'slide A#0     22
+#Kick drum: C-1         0
+#Cowbell F-1            5
+#Snare C#-1             1
+#Clap D-1               2
+#Scrape'n'slide B-1     11
+#Open Hat E-1           4
+#Closed Hat D#-1        3
+#Tom G-1                7
 
 import random
 
 instruments = (
-    41, 49, 63, 1, 22
-)
-
-probabilities = (
-    0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0
+    (
+        "kick", #Name
+        0,  # MIDI note
+        (0.8, 0.1, 0.4, 0.1, 0.8, 0.1, 0.4, 0.1, 0.8, 0.1, 0.4, 0.1, 0.8, 0.1, 0.4, 0.1),   #Hit probability for each 16th note in measure
+        (1, 0)  #Relative probability for hit if not previously hit, previously hit
+    ),
+    (
+        "snare",
+        1,
+        (0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2),
+        (1, 0.5)
+    ),
+    (
+        "closed hat",
+        3,
+        (0.2, 0.75, 0.2, 0.75, 0.2, 0.75, 0.2, 0.75, 0.2, 0.75, 0.2, 0.75, 0.2, 0.75, 0.2, 0.75),
+        (1, 3)
+    ),
+    (
+        "open hat",
+        4,
+        (0.5, 0.1, 0.2, 0.1, 0.5, 0.1, 0.2, 0.1, 0.5, 0.1, 0.2, 0.1, 0.5, 0.1, 0.2, 0.1),
+        (1, 0)
+    ),
+    (
+        "tom",
+        7,
+        (0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2, 0.75, 0.2, 0.5, 0.2),
+        (1, 0.5)
+    ),
+    (
+        "clap",
+        2,
+        (0.2, 0.3, 0.5, 0.3, 0.2, 0.3, 0.5, 0.3, 0.2, 0.3, 0.5, 0.3, 0.2, 0.3, 0.5, 0.2),
+        (1, 1.5)
+    )
 )
 
 notes = []
@@ -24,11 +58,17 @@ def init():
 
     notes = []
     rhythym = []
+
+    prevHits = [0] * len(instruments)
     for i in range(16):
         attacks = []
         for instrument in range(len(instruments)):
-            if (random.random() + probabilities[i]) > 1.0:
-                attacks.append(instruments[instrument])
+            prob = instruments[instrument][2][i] * instruments[instrument][3][prevHits[instrument]]
+            if (random.random() + prob) > 1.0:
+                attacks.append(instruments[instrument][1])
+                prevHits[instrument] = 1
+            else:
+                prevHits[instrument] = 0
 
         if len(attacks) > 0:
             notes.append(attacks)
