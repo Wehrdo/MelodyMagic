f0e6c560afd0af04792b049641132c03ac0b88e4
David Wehr <wehr_d@hotmail.com>
2018-03-23 22:25:14 -0500

    Simple melody generation

--- source/core.py
+++ source/core.py
@@ -9,6 +9,10 @@ def make_chord_msgs(chord, key, vel=100, transposition=0):
         messages.append(mido.Message('note_on', note=key+note, velocity=vel))
     return messages
 
+def octave_to_note(octave, note_offset):
+    octave_root = 12 * octave
+    return octave_root + note_offset
+
 def run(out_port):
     melody_gen = MelodyGen()
     transposition = 0
@@ -21,12 +25,19 @@ def run(out_port):
         for msg in make_chord_msgs(chord, key, 100, transposition):
             out_port.send(msg)
 
-        # melody_notes, melody_rhythms = melody_gen.get_next()
+        melody_notes, melody_rhythms = melody_gen.get_next(chord)
+        for note_pair, duration in zip(melody_notes, melody_rhythms):
+            midi_note = octave_to_note(note_pair[1], note_pair[0])
+            start_msg = make_chord_msgs([midi_note], key, 100, transposition)[0]
+            out_port.send(start_msg)
+            time.sleep(s_per_sixteenth * duration)
+            end_msg = make_chord_msgs([midi_note], key, 0, transposition)[0]
+            out_port.send(end_msg)
 
-        for beat in range(1, 4):
-            for sixteenth in range(16):
-                # TODO: Run a global timer instead of sleep
-                time.sleep(s_per_sixteenth)
+        # for beat in range(1, 4):
+        #     for sixteenth in range(16):
+        #         # TODO: Run a global timer instead of sleep
+        #         time.sleep(s_per_sixteenth)
 
         for msg in make_chord_msgs(chord, key, 0, transposition):
             out_port.send(msg)
--- source/melody.py
+++ source/melody.py
@@ -1,18 +1,46 @@
 import random
+import math
+import numpy as np
+import scipy.stats
 
 class MelodyGen:
     def __init__(self):
-        self.octave = 4
+        self.octave = 0
         self.chord_idx = 0
         pass
 
     def get_next(self, chord):
+        # Generate Markov model
+        n_octaves = 2
+        dist = scipy.stats.norm
+        dim = n_octaves * len(chord)
+        mm = np.zeros((dim, dim))
+        # center = int(dim/2)
+        for i in range(dim):
+            for j in range(dim):
+                mm[i,j] = dist.cdf(j, loc=i, scale=dim/2) - \
+                          dist.cdf(j-1, loc=i, scale=dim/2)
+        print(mm)
+
         notes = []
         rhythms = []
 
         duration = 0
         measure_duration = 16*4
         while duration < measure_duration:
+            # Choose note duration
             note_length = random.randint(0, min(measure_duration - duration, 16))
+            duration += note_length
+            rhythms.append(note_length)
+
+            # Step random direction
+            idx_step = random.randint(-1, 1)
+            self.chord_idx += idx_step
+            # Move octave if necessary
+            self.octave += math.floor(self.chord_idx / len(chord))
+            # Wrap chord idx
+            self.chord_idx = self.chord_idx % len(chord)
+
+            notes.append((chord[self.chord_idx], self.octave))
 
         return notes, rhythms
